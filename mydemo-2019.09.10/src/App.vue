e0bfe3dab4894ff89571e77ec2dcee7287ce8763
wangbinze <wangbinze1994@gmail.com>
2019-09-15 21:31:48 +0800

    0

--- mydemo-2019.09.10/src/App.vue
+++ mydemo-2019.09.10/src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <h1>这是app的h1</h1>
+    <div class="card" style="width:18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">111111111111111111111222222</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  created() {
+    this.$http.get("https://randomuser.me/api/").then(response => {
+      console.log(response.data);
+    });
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +38,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+// $color:black;
+// body{
+//   background-color: $color;
+// }
+
+// @import "~bootstrap/scss/bootstrap";
 </style>
--- mydemo-2019.09.10/src/main.js
+++ mydemo-2019.09.10/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import axios from 'axios' //主要的ajax套件
+import VueAxios from 'vue-axios'  //将它转为Vue的套件
 import App from './App.vue'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
