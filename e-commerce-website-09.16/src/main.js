a0179537d8426aabb87146db089120e3d7ad0939
wangbinze <wangbinze1994@gmail.com>
2019-09-16 23:49:49 +0800

    0

--- e-commerce-website-09.16/src/main.js
+++ e-commerce-website-09.16/src/main.js
@@ -14,3 +14,25 @@ new Vue({
     render: h => h(App),
     router
 }).$mount('#app')
+
+router.beforeEach((to, from, next) => {
+    console.log('to', to, 'from', from, 'next', next)
+    //验证登录的，登录信息保存后才可进入到主页等其他页面
+    if (to.meta.requiresAuth) {
+        const api = "https://vue-course-api.herokuapp.com/api/user/check";
+        axios.post(api).then((response) => {
+            console.log(response.data);
+            if (response.data.success) {
+                // vm.$router.push('/');
+                next();
+            }else{
+                next({
+                    path:'/login',
+                })
+            }
+        });
+        // console.log('这里需要验证')
+    } else {
+        next();
+    }
+})
--- e-commerce-website-09.16/src/router/index.js
+++ e-commerce-website-09.16/src/router/index.js
@@ -3,19 +3,42 @@ import Router from 'vue-router';
 //官方的元件
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/pages/Login'
+import Dashboard from '@/components/Dashboard'
+import Products from '@/components/pages/Products'
 
 
 Vue.use(Router);
 
 export default new Router({
+
     // mode: 'history',
     routes: [{
+        path: '*',
+        redirect: 'login',   //避免用于输入其他路径进入空白页
+    }, {
         name: 'HelloWorld', //组件呈现的名称
         path: '/', //对应的虚拟路径
         component: HelloWorld, //对应的组件
+        meta: {
+            requiresAuth: true
+        },
     }, {
         name: 'Login',
         path: '/login',
         component: Login,
+    },{
+        name: 'Dashboard',
+        path: '/admin',
+        component: Dashboard,
+        children:[
+            {
+                name: 'Products',
+                path: 'products',
+                component: Products,
+                meta: {
+                    requiresAuth: true
+                },
+            },
+        ]
     }]
 })
