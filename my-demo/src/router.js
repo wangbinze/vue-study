c3f3f1d3d67a1a39714aa995d07a7a02a1e1ad04
binze <40032420+wangbinze@users.noreply.github.com>
2019-11-22 14:33:59 +0800

    删除

--- my-demo/src/router.js
+++ my-demo/src/router.js
@@ -1,40 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import HelloWorld from './components/HelloWorld.vue'
-// import Player from './components/Player';
-// import PlayerProfile from './components/Player/Profile.vue';
-// import PlayerStats from './components/Player/Stats.vue'
-
-import SettingHeader from './components/setting/Header';
-import SettingSidebar from './components/setting/Sidebar';
-import SettingDetail from './components/setting/Detail';
+import HelloWorld from './components/HelloWorld.vue'
 
 Vue.use(Router);
 
 export default new Router({
     routes: [{
-            path: '/',
-            name: 'HelloWorld',
-            // component: HelloWorld
-            components: {
-                myHeader: SettingHeader,
-                mySidebar: SettingSidebar,
-                myDetail: SettingDetail
-            }
-        },
-        // {
-        //     path: '/player/:uid',
-        //     name: 'Player',
-        //     component: Player,
-        //     children: [{
-        //             path: 'profile',
-        //             component: PlayerProfile
-        //         },
-        //         {
-        //             path: 'stats',
-        //             component: PlayerStats
-        //         }
-        //     ]
-        // }
-    ]
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    }, ]
 })
